c30e3df72e974534452d2ca3a8ce1906588fc008
Dmitry Misiulia <d.misiulia@hackspace.by>
2016-11-10 15:40:42 +0300

    Added Logs in rest

--- app/src/main/java/com/misyulya/rssnewsapplication/rest/RestFactory.java
+++ app/src/main/java/com/misyulya/rssnewsapplication/rest/RestFactory.java
@@ -10,10 +10,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class RestFactory {
 
-    public static ApiInterface get(){
+    private static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor).build();
+
+    public static ApiInterface get() {
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(ApiRss.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         ApiInterface service = retrofit.create(ApiInterface.class);
         return service;
